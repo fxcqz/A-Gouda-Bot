de70045ae5af51beaddb223f48a97bfe3dfe1d6d
Matthew Rawcliffe <mar44@aber.ac.uk>
2013-12-04 23:45:08 +0000

    formatting no class, better syntax

--- lib/core/formatting.py
+++ lib/core/formatting.py
@@ -1,10 +1,8 @@
 import ast
 import os.path
 
-class Formatting:
-    def __init__(self):
-        self.formats = {'black': '\x1b[30m', 'red': '\x1b[31m', 'green': '\x1b[32m',
-        'yellow': '\x1b[33m', 'blue': '\x1b[34m', 'purple': '\x1b[35m', 'cyan': '\x1b36m'}
+formats = {'black': '\x1b[30m', 'red': '\x1b[31m', 'green': '\x1b[32m',
+'yellow': '\x1b[33m', 'blue': '\x1b[34m', 'purple': '\x1b[35m', 'cyan': '\x1b36m'}
 
-    def fmat(self, string, fmat):
-        return str(self.formats[fmat] + string + '\x1b[0m')
+def fmat(string, fmat):
+    return str(formats[fmat] + string + '\x1b[0m')
