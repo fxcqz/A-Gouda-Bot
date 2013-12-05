95c7573791aab1bc58880a87bc4a6ee992e8a12c
rabolton <rob.a.bolton@gmail.com>
2013-12-05 00:04:11 +0000

    Fixed a missing square brace from cyan\'s string

--- lib/core/formatting.py
+++ lib/core/formatting.py
@@ -2,7 +2,7 @@ import ast
 import os.path
 
 formats = {'black': '\x1b[30m', 'red': '\x1b[31m', 'green': '\x1b[32m',
-'yellow': '\x1b[33m', 'blue': '\x1b[34m', 'purple': '\x1b[35m', 'cyan': '\x1b36m'}
+'yellow': '\x1b[33m', 'blue': '\x1b[34m', 'purple': '\x1b[35m', 'cyan': '\x1b[36m'}
 
 def fmat(string, fmat):
     return str(formats[fmat] + string + '\x1b[0m')
