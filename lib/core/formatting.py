957e3f8950410bed664aa229aad36030cb1af68a
tiggy <tiggy@capensis.(none)>
2013-12-05 14:14:32 +0000

    Formatting fixes, updated to contain more colours etc, tag replace function.

--- lib/core/formatting.py
+++ lib/core/formatting.py
@@ -3,13 +3,15 @@ import os.path
 
 formats = {
 'black': '\x1b[30m', 
-'red': '\x1b[31m', 'green': '\x1b[32m',
+'red': '\x1b[31m',
+'green': '\x1b[32m',
 'yellow': '\x1b[33m',
 'blue': '\x1b[34m',
 'purple': '\x1b[35m',
 'cyan': '\x1b[36m',
 'black_bg': '\x1b[40m', 
-'red_bg': '\x1b[41m', 'green': '\x1b[32m',
+'red_bg': '\x1b[41m',
+'green': '\x1b[32m',
 'yellow_bg': '\x1b[43m',
 'blue_bg': '\x1b[44m',
 'purple_bg': '\x1b[45m',
@@ -17,15 +19,15 @@ formats = {
 'bold': '\x1b[1m',
 'underline': '\x1b[4m',
 'flash': '\x1b[5m',
-'clear': '\x1b[0m'}
+'clear': '\x1b[21;0m'}
 tag_left = "["
 tag_right = "]"
 
 def fmat(string, fmat):
     return str(formats[fmat] + string + '\x1b[0m')
 
-def fmatTags(string):
+def fmat_tags(string):
     return_string = string
-    for tag, ctrl_code in formats:
+    for tag, ctrl_code in formats.iteritems():
         return_string = return_string.replace(tag_left + tag + tag_right, ctrl_code)
     return return_string
