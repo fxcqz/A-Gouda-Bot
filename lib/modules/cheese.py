b5a4db2ae177e07aaf1992d91b671ee35f59254b
rabolton <rob.a.bolton@gmail.com>
2013-12-05 00:03:27 +0000

    Added link functionality and usage of formatting module to cheese.py

--- lib/modules/cheese.py
+++ lib/modules/cheese.py
@@ -1,13 +1,28 @@
 import re
 import urllib2
+import os,sys
+sys.path.append(os.path.abspath('../core/'))
+import formatting
 
 def cotd():
-    response = urllib2.urlopen("http://cheese.com")
+    cheese_url = "http://cheese.com"
+    failed_errortext = "I was unable to find the cheese of the day."
+    response = urllib2.urlopen(cheese_url)
     if response:
-        m = re.search( r'style="color:.*">(.*)</a></h4>', response.read(), re.M|re.I)
-        if m:
-            return "The cheese of the day is " + m.group(1) + "\r\n"
-    return "Unable to get cheese of the day"
+        cotd_url = re.search( r'<h4>Cheese of the day:</h4>.*<div class="more" onclick="location.href=\'(.*)\'">more</div>', response.read(), re.DOTALL|re.M|re.I).group(1)
+        cotd_page = urllib2.urlopen(cheese_url + cotd_url)
+        if cotd_page:
+            cpr = cotd_page.read()
+            cotd_name_match = re.search( r'<title>(.*)</title>', cpr)
+            if not cotd_name_match:
+                return failed_errortext + ": Error obtaining name"
+            cotd_name = cotd_name_match.group(1)
+            cotd_description_match = re.search( r'<meta name="description" content="(.*)"/>', cpr)
+            if not cotd_description_match:
+                return failed_errortext + ": Error obtaining description"
+            cotd_description = cotd_description_match.group(1)
+            return "The cheese of the day is " + formatting.fmat(cotd_name, "yellow") + " More Info: " + formatting.fmat(cheese_url + cotd_url, "cyan")  + "\r\n"
+    return failed_errortext
 
 def get_args():
     arglist = ["cotd"]
