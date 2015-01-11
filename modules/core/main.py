e5f94067b455117e6e74a85bef55eb7966ea31e4
mrawcliffe <mar44@aber.ac.uk>
2015-01-10 22:05:31 -0500

    added option to list all modules loaded

--- modules/core/main.py
+++ modules/core/main.py
@@ -1,5 +1,15 @@
+def get_modules(handler):
+    ret = "No modules loaded"
+    if handler.importlist:
+        modules = [key for key in handler.importlist]
+        ret = ', '.join(modules)
+    return ret
+
 def main(irc, nick, data, handler):
     if data[0][:-1] == "Gouda" and len(data) > 1:
         # message is addressed to the bot
         if data[1] == "commands":
             irc.message("You can't command me, I'm a free spirit!")
+
+        if data[1] == "modules":
+            irc.message("Modules currently loaded are: " + get_modules(handler))
