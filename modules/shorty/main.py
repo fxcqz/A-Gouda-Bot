1d5756a22e8fcff99e0fd81c12bd895c9614f093
Rob <badger.ea@gmail.com>
2015-04-09 13:05:09 +0100

    Fixed?

--- modules/shorty/main.py
+++ modules/shorty/main.py
@@ -1,25 +1,33 @@
 import re
-import requests
+import urllib
+import urllib2
 import json
 import random
 
 url_re = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-shortern_url = 'http://www.googleapis.com/urlshortener/v1/url'
+shorten_domain = 'bitly.com'
+oauthkey = 'c302a13fae88967b205cef85de65666fd65a413d'
 errors = ['fuck', 'shit', 'wanker', 'bastard', 'oh bollocks', 'shitting hell', 'cunting internet']
 creepy = [';)', 'no tears... only dreams now...']
 
 def main(irc, nick, data, handler):
+    print("irc: " + irc + ", nick: " + nick + ", data: " + data)
     urls = url_re.findall(data)
+    print(urls)
     if urls:
-        if not "shh..." in data:
+        if "shh..." not in data:
             return_urls = []
             for url in urls:
-                json_postdata = {'longUrl': url}
                 try:
-                    response = requests.post(shorten_url, data=json_postdata).json
-                    return_urls.append(response['id'])
-                except (ValueError, ConnectionError):
+                    full_url = 'https://api-ssl.bitly.com/v3/shorten?' + urllib.urlencode({
+                        'ACCESS_TOKEN': oauthkey,
+                        'longUrl': url,
+                    })
+                    body = json.loads(urllib2.urlopen(full_url).read())
+                    return_urls.append(body['data']['url'])
+                except (ValueError, httplib.HTTPException) as ex:
+                    print(ex)
                     return_urls.append(random.choice(errors) + "...")
-            irc.message(return_urls.join(', '))
+            irc.message(", ".join(return_urls))
         else:
             irc.message(random.choice(creepy))
